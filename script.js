32478289d822f99c1b1580996fcb1330bf647c66
Stray <strayfrozen@yahoo.com>
2022-08-02 13:28:59 -0500

    set up a caculator class also set up empty functions also added updateDisplay function

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
+    }
+  
+    clear() {
+     
+    }
+  
+    delete() {
+     
+    }
+  
+    appendNumber(number) {
+      
+    }
+  
+    chooseOperation(operation) {
+     
+    }
+  
+    compute() {
+      
+    }
+  
+    getDisplayNumber(number) {
+    
+    }
+  
+    updateDisplay() {
+      this.currentOperandTextElement.innerText =
+        this.getDisplayNumber(this.currentOperand)
+      if (this.operation != null) {
+        this.previousOperandTextElement.innerText =
+          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      } else {
+        this.previousOperandTextElement.innerText = ''
+      }
+    }
+  }
+  
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
